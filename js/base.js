6f21e01676f2cef8d3c8fbd822cde5442ed1876b
vineetkumarraj <kumarrajvineet@gmail.com>
2020-06-02 20:18:19 +0530

    Uploading code from local to git

---
+++ js/base.js
@@ -0,0 +1,11 @@
+$(function () {	
+	$('.subnavbar').find ('li').each (function (i) {
+	
+		var mod = i % 3;
+		
+		if (mod === 2) {
+			$(this).addClass ('subnavbar-open-right');
+		}
+		
+	});	
+});
